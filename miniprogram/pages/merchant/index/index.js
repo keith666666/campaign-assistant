d0acb44750cd39b0d780ac5e2de16a122578996e
keith <keith.huang@walkthechat.com>
2019-11-06 20:17:40 +0800

    add merchant index page

---
+++ miniprogram/pages/merchant/index/index.js
@@ -0,0 +1,75 @@
+// miniprogram/pages/merchant/index/index.js
+const app = getApp()
+let indexController = require('../../../controllers/index-controller');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    indexController.login('merchant', (err, user) => {
+      if (err) console.log(err);
+      else {
+        app.globalData.merchant = user;
+        console.log(user);
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
